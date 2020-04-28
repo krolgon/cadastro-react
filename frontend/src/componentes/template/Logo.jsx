a8ba3605666f3bccfebb84a6fec3cd410e8ebf7e
krolgon <kah.goncalvez@gmail.com>
2020-04-27 21:03:33 -0300

    componente logo

--- frontend/src/componentes/template/Logo.jsx
+++ frontend/src/componentes/template/Logo.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import './Logo.css';
+import logo from '../../assets/imagens/logo.png';
 
 const Logo = (props) => {
-  return <aside className='logo'>Logo</aside>;
+  return (
+    <aside className='logo'>
+      <a href='#' className='logo'>
+        <img src={logo} alt='logo' />
+      </a>
+    </aside>
+  );
 };
 
 export default Logo;
