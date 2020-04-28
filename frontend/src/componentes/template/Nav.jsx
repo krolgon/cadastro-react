7e7f9cde1835a043d7ac6064867209cc955ed15d
krolgon <kah.goncalvez@gmail.com>
2020-04-28 10:09:14 -0300

    componente navegação

--- frontend/src/componentes/template/Nav.jsx
+++ frontend/src/componentes/template/Nav.jsx
@@ -2,7 +2,18 @@ import React from 'react';
 import './Nav.css';
 
 const Nav = (props) => {
-  return <aside className='menu-area'>Navegação</aside>;
+  return (
+    <aside className='menu-area'>
+      <nav className='menu'>
+        <a href='#'>
+          <i className='fa fa-home'></i> Início
+        </a>
+        <a href='#/users'>
+          <i className='fa fa-users'></i> Usuários
+        </a>
+      </nav>
+    </aside>
+  );
 };
 
 export default Nav;
